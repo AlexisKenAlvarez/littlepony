b4b978f287cc59d7c7336a7ab32fd38e6343ca88
Alexis Ken Alvarez <alexisken1432@gmail.com>
2023-06-12 10:32:51 +0800

    added transition for hover nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -23,7 +23,13 @@ const Nav = () => {
             <ul className="flex w-fit ml-auto gap-x-28 mr-20">
                 {navlist.map((items, index) => {
                     return (
-                        <li className="" key={index}>{items.label}</li>
+                        <li className="relative group" key={index}>
+                            <button className="">
+                                {items.label}
+                            </button>
+
+                            <div className="absolute h-[4px] left-0 right-0 mx-auto -bottom-[5px] rounded-md bg-gradient-to-r from-myPink via-[#FEB958] to-[#FD6E50] w-0 group-hover:w-full transition-width ease-in-out duration-300"></div>
+                        </li>
                     )
                 })}
             </ul>
