6213c8f612d26343f1dcc8f0342822f0fbf25d18
Alexis Ken Alvarez <alexisken1432@gmail.com>
2023-06-12 11:12:59 +0800

    added sparkles

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,34 +1,47 @@
+import { FaHome } from 'react-icons/fa'
+import { TiInfoLarge } from 'react-icons/ti'
+import { GiToken } from 'react-icons/gi'
+import { RiRoadMapFill } from 'react-icons/ri'
+
 const Nav = () => {
     const navlist = [
         {
             label: 'Home',
-            slug: ''
+            slug: '',
+            icon: <FaHome />
         },
         {
             label: 'About',
-            slug: ''
+            slug: '',
+            icon: <TiInfoLarge />
         },
         {
             label: 'Tokenomics',
-            slug: ''
+            slug: '',
+            icon: <GiToken />
         },
         {
             label: 'Roadmap',
-            slug: ''
+            slug: '',
+            icon: <RiRoadMapFill />
         },
-
     ]
     return (
         <nav className="font-poppins z-20 absolute top-10 max-w-[1500px] px-5 mx-auto left-0 right-0">
-            <ul className="flex w-fit ml-auto gap-x-28 mr-20">
+            <ul className="flex w-fit lg:ml-auto xl:gap-x-24 lg:gap-x-16 gap-x-10 lg:mr-20 mx-auto">
                 {navlist.map((items, index) => {
                     return (
                         <li className="relative group" key={index}>
-                            <button className="">
+                            <button className="lg:flex hidden items-center gap-x-[3px]">
+                                {items.icon}
                                 {items.label}
                             </button>
 
-                            <div className="absolute h-[4px] left-0 right-0 mx-auto -bottom-[5px] rounded-md bg-gradient-to-r from-myPink via-[#FEB958] to-[#FD6E50] w-0 group-hover:w-full transition-width ease-in-out duration-300"></div>
+                            <div className="lg:block hidden absolute h-[4px] left-0 right-0 mx-auto -bottom-[5px] rounded-md bg-gradient-to-r from-myPink via-[#FEB958] to-[#FD6E50] w-0 group-hover:w-full transition-width ease-in-out duration-300"></div>
+
+                            <div className="lg:hidden block text-lg">
+                                {items.icon}
+                            </div>
                         </li>
                     )
                 })}
--- src/views/Hero/HeroIndex.tsx
+++ src/views/Hero/HeroIndex.tsx
@@ -33,30 +33,33 @@ const HeroIndex = () => {
             <Nav />
 
 
-            <img src="/circle2.webp" alt="Circle" className="absolute bottom-0 right-5 w-[20rem] translate-y-1/2" />
-            <img src="/rainbow.webp" alt="Rainbow" className="absolute top-0 left-0 z-10" />
+            <img src="/circle2.webp" alt="Circle" className="absolute bottom-0 right-5 md:w-[20rem] w-[10rem] translate-y-1/2" />
+            <img src="/rainbow.webp" alt="Rainbow" className="absolute top-0 left-0 z-10 md:w-auto w-20" />
 
-            <div className="absolute bottom-0 left-0 w-[50%] h-full flex">
+            <div className="absolute bottom-0 left-0 w-[50%] h-full lg:flex hidden">
                 <motion.img initial={{ y: 50, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 0.1 }} src="/Unisplit/1.webp" alt="1" className="" />
                 <motion.img initial={{ y: 50, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 0.3 }} src="/Unisplit/2.webp" alt="2" className="" />
                 <motion.img initial={{ y: 50, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1, delay: 0.6 }} src="/Unisplit/3.webp" alt="3" className="" />
             </div>
 
             <div className="max-w-[1500px] w-full h-full flex mx-auto relative z-10">
-                <div className="w-full"></div>
+                <div className="w-full lg:block hidden"></div>
                 <div className="w-full flex  flex-col justify-center">
-                    <div className="relative w-fit h-fit">
-                        <h1 className="font-modak text-cream text-8xl font-outline-2 z-[5] relative">My Little Pony</h1>
+                    <div className="relative w-fit h-fit mx-auto lg:mx-0 md:text-8xl sm:text-5xl text-4xl">
+                        <img src="/sparkle.gif" alt="" className="absolute w-24 z-10 -top-10 -left-10" />
+                        <img src="/sparkle.gif" alt="" className="absolute w-24 z-10 -bottom-5 -right-10" />
+
+                        <h1 className="font-modak text-cream md:text-8xl font-outline-2 z-[5] relative">My Little Pony</h1>
                         <motion.div initial="hidden" animate="show" variants={container} className="">
-                            <motion.h1 variants={rainbowVariant} className="font-modak text-[#FEB958] text-8xl font-outline-2 absolute z-[4] -top-[7px] tracking-[-.5px] mx-auto left-0 right-0 w-fit">My Little Pony</motion.h1>
-                            <motion.h1 variants={rainbowVariant} className="font-modak text-[#FD6E50] text-8xl font-outline-2 absolute z-[3] -top-[14px] tracking-[-1px] mx-auto left-0 right-0 w-fit">My Little Pony</motion.h1>
-                            <motion.h1 variants={rainbowVariant} className="font-modak text-[#E15A8C] text-8xl font-outline-2 absolute z-[2] -top-[21px] tracking-[-1.5px] mx-auto left-0 right-0 w-fit">My Little Pony</motion.h1>
-                            <motion.h1 variants={rainbowVariant} className="font-modak text-[#BB52F6] text-8xl font-outline-2 absolute z-[1] -top-[28px] tracking-[-2px] mx-auto left-0 right-0 w-fit">My Little Pony</motion.h1>
+                            <motion.h1 variants={rainbowVariant} className="font-modak text-[#FEB958] font-outline-2 absolute z-[4] sm:-top-[7px] -top-[4px] tracking-[-.5px] mx-auto left-0 right-0 w-fit">My Little Pony</motion.h1>
+                            <motion.h1 variants={rainbowVariant} className="font-modak text-[#FD6E50] font-outline-2 absolute z-[3] sm:-top-[14px] -top-[8px] tracking-[-1px] mx-auto left-0 right-0 w-fit">My Little Pony</motion.h1>
+                            <motion.h1 variants={rainbowVariant} className="font-modak text-[#E15A8C] font-outline-2 absolute z-[2] sm:-top-[21px] -top-[12px] tracking-[-1.5px] mx-auto left-0 right-0 w-fit">My Little Pony</motion.h1>
+                            <motion.h1 variants={rainbowVariant} className="font-modak text-[#BB52F6] font-outline-2 absolute z-[1] sm:-top-[28px] -top-[16px] tracking-[-2px] mx-auto left-0 right-0 w-fit">My Little Pony</motion.h1>
                         </motion.div>
 
                     </div>
 
-                    <div className="flex flex-col gap-y-4 mt-4">
+                    <div className="flex flex-col gap-y-4 mt-4 text-center lg:text-left md:text-base text-sm">
                         <Appear>
                             <p className="">
                                 <span className="text-2xl font-semibold">Welcome</span> to the magical world of My Little Pony on the Ethereum blockchain, where <span className="font-semibold text-myPink italic">$FRIENDSHIP</span> takes us on a thrilling journey on Equestria, from Ponyville to moonshot Canterlot!
@@ -69,13 +72,13 @@ const HeroIndex = () => {
                     </div>
 
                     <Appear delay={0.4}>
-                        <div className="flex mt-10">
-                            <button className="max-w-[10rem] w-full py-3 rounded-md bg-gradient-to-br from-myPink to-myIndigo text-white font-poppins font-semibold text-sm mt-8 self-end">
+                        <div className="flex mt-10 mx-auto w-fit z-10 lg:mx-0">
+                            <button className="max-w-[10rem] w-full py-3 rounded-md bg-gradient-to-br from-myPink to-myIndigo text-white font-poppins font-semibold text-sm mt-8 self-end flex-shrink-0">
                                 <a href="" className="">
                                     BUY TOKEN
                                 </a>
                             </button>
-                            <img src="/cute.webp" alt="Cute" className="w-28 -ml-7" />
+                            <img src="/cute.webp" alt="Cute" className="sm:w-28 w-20 sm:-ml-7 -ml-2" />
                         </div>
                     </Appear>
 
